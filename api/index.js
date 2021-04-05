258497e3958bfd54f9cc478ee92049545474fc63
Paulus <elxebec@gmail.com>
2021-04-05 15:25:54 -0300

    readme agregado

--- api/index.js
+++ api/index.js
@@ -1,52 +1,18 @@
 const express = require("express");
-const axios = require("axios");
-const cors = require ("cors");
+const cors = require("cors");
 const morgan = require("morgan");
-
+const routes = require("./routes");
 const app = express();
 //==========================================//
-
+// Middlewares
 app.use(cors());
-app.use(express.json())
-app.use(morgan("dev"))
+app.use(express.json());
+app.use(morgan("dev"));
 
 //==========================================//
 
-app.get("/",(req,res)=>{
-    res.json({"Hola":"Hola"})
-})
+app.use("/", routes);
 
-app.get("/quote",async(req,res)=>{
-    const data = await axios(
-        'https://www.breakingbadapi.com/api/quote/random'
-        )
-    res.json(data.data[0])
-})
-
-app.get("/characters?name",async(req,res)=>{
-    const name = req.query.name;
-    const data = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
-    res.json(data)
-})
-
-app.get("/characters/:id", async(req,res)=>{
-    const id = Number(req.params.id);
-    const data = await axios(`https://www.breakingbadapi.com/api/characters/${id}`);
-    res.json(data.data)
-})
-
-app.get("/episodes", async(req,res)=>{
-    const data = await axios ("https://www.breakingbadapi.com/api/episodes")
-    res.json(data.data)
-})
-
-app.get("/episodes/:id", async(req,res)=>{
-    const id = Number(req.params.id);
-    const data = await axios( `https://www.breakingbadapi.com/api/episodes/${id}`);
-    res.json(data.data)
-})
-
-
-
-
-app.listen(3001,()=>console.log("Escuchando puerto 3001"))
+app.listen(3000, () =>
+  console.log("¡Aplicación de ejemplo escuchando en el puerto 3000!")
+);
